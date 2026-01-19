152c0ba9b31129bfb0923f047a40ae6c7a230808
Manika Kutiyal <kutiyalmanika@gmail.com>
2026-01-19 13:13:12 +0530

    URI update

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const cartSchema = new mongoose.Schema({
-  items: [
-    {
-      id: String,
-      name: String,
-      price: Number,
-      image: String,
-      variant: String,
-      quantity: {
-        type: Number,
-        default: 1
-      }
-    }
-  ]
-});
+// const cartSchema = new mongoose.Schema({
+//   items: [
+//     {
+//       id: String,
+//       name: String,
+//       price: Number,
+//       image: String,
+//       variant: String,
+//       quantity: {
+//         type: Number,
+//         default: 1
+//       }
+//     }
+//   ]
+// });
 
-module.exports = mongoose.model("Cart", cartSchema);
+// module.exports = mongoose.model("Cart", cartSchema);
